462edd25648688c10cb420d1a28bed9bd7ac991e
Ben Wells-Day <benwellsday@gmail.com>
2020-08-24 23:01:14 +0100

    test for convolution1d

---
+++ test/transformer/layer/tconvolution1d.m
@@ -0,0 +1,38 @@
+classdef tconvolution1d < matlab.unittest.TestCase
+    % tconvolution1d   Tests for the convolution1d function
+    
+    % Copyright 2020 The MathWorks, Inc.
+    
+    properties(Constant,Access=private)
+        convolution1d = @transformer.layer.convolution1d
+    end
+    
+    properties(TestParameter)
+        Input = iInput
+    end
+    
+    methods(Test)
+        function hasExpectedValue(test,Input)
+            x = dlarray(Input.x);
+            W = dlarray(Input.W);
+            b = dlarray(Input.b);
+            z_exp = fullyconnect(x,W,b,'DataFormat','CB');
+            z_act = test.convolution1d(x,W,b);
+            test.verifyEqual(z_act,z_exp);
+        end
+    end
+end
+
+function s = iInput()
+s = struct(...
+    'OneChannel',iInputCase(1,2,0),...
+    'TwoChannel',iInputCase(ones(2,1),rand(3,2),rand(3,1)),...
+    'MultipleObservations',iInputCase(rand(3,4),rand(5,3),rand(5,1)));
+end
+
+function s = iInputCase(x,w,b)
+s = struct(...
+    'x',x,...
+    'W',w,...
+    'b',b);
+end
