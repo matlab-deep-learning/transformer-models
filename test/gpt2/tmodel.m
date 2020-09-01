b5fa0178cebe847142521b8478a63a6376f6ed08
Ben Wells-Day <benwellsday@gmail.com>
2020-09-01 17:48:55 +0100

    add tests for gpt2.model

---
+++ test/gpt2/tmodel.m
@@ -0,0 +1,40 @@
+classdef(SharedTestFixtures = {downloadGPT2Fixture}) tmodel < matlab.unittest.TestCase
+    % tmodel   Tests for gpt2.model
+    
+    % Copyright 2020 The MathWorks, Inc.
+    
+    properties(Constant)
+        model = @gpt2.model
+    end
+    
+    methods(Test)
+        function canUseModel(test)
+            inputs = test.prepareInputs();
+            test.verifyWarningFree(@() test.model(inputs{:}));
+        end
+    end
+    
+    methods(Access=private)
+        function inputs = prepareInputs(test)
+            % Convenience method to setup inputs for
+            % transformer.model
+            X = test.prepareX();            
+            parameters = test.prepareParameters();
+            pasts = test.preparePasts(parameters.Hyperparameters.NumLayers);
+            inputs = {X,pasts,parameters};
+        end
+        
+        function X = prepareX(~)
+            X = dlarray(1);
+        end
+        
+        function pasts = preparePasts(~,numLayers)
+            pasts = cell(numLayers,1);
+        end
+        
+        function parameters = prepareParameters(~)
+            parametersFile = fullfile(getRepoRoot(),'gpt2-355M','parameters.mat');
+            parameters = gpt2.load(parametersFile);
+        end
+    end
+end
